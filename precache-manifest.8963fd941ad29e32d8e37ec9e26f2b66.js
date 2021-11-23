50011853131b2267db28e79318ebb1670bac0fce
vramdal <>
2021-11-23 21:23:12 +0000

    Deploying to gh-pages from @ vramdal/trykktap@80432695bd831c86d31a87c35d242ee90f74f81b 🚀

---
+++ precache-manifest.8963fd941ad29e32d8e37ec9e26f2b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea361aeadf13f42981c8cd0b37426435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c2c6c6366bf8777c95",
+    "url": "/static/css/main.8d483b34.chunk.css"
+  },
+  {
+    "revision": "b7f26d64f11c8aed458a",
+    "url": "/static/js/2.776aa229.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.776aa229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c2c6c6366bf8777c95",
+    "url": "/static/js/main.e0dd292b.chunk.js"
+  },
+  {
+    "revision": "f5773a846fa85c0fed91",
+    "url": "/static/js/runtime-main.e3f57e3d.js"
+  }
+]);
